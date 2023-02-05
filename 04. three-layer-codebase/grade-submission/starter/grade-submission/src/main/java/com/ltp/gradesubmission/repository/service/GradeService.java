2a4b6a8c5efba7a50f5a5bdf2e3a3eefbf37b3fd
Jackson Weber <jackson@jackcode.com>
2023-02-05 12:32:59 -0500

    refactored into three layers

--- 04. three-layer-codebase/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/repository/service/GradeService.java
+++ 04. three-layer-codebase/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/repository/service/GradeService.java
@@ -1,5 +1,50 @@
 package com.ltp.gradesubmission.repository.service;
 
-public class GradeService {
+import java.util.List;
 
+import com.ltp.gradesubmission.Constants;
+import com.ltp.gradesubmission.Grade;
+import com.ltp.gradesubmission.repository.GradeRepository;
+
+public class GradeService {
+  GradeRepository gradeRepository = new GradeRepository();
+
+  public Grade getGrade(int index) {
+    return this.gradeRepository.getGrade(index);
+  }
+
+  public void addGrade(Grade grade) {
+    this.gradeRepository.addGrade(grade);
+  }
+
+  public void updateGrade(int index, Grade grade) {
+    this.gradeRepository.updateGrade(index, grade);
+  }
+
+  public List<Grade> getGrades() {
+    return this.gradeRepository.getGrades();
+  }
+
+  public int getGradeIndex(String id) {
+    List<Grade> studentGrades = this.getGrades();
+    for (int i = 0; i < studentGrades.size(); i++) {
+      if (studentGrades.get(i).getId().equals(id))
+        return i;
+    }
+    return Constants.NOT_FOUND;
+  }
+
+  public Grade getGradeById(String id) {
+    int index = this.getGradeIndex(id);
+    return index == Constants.NOT_FOUND ? new Grade() : this.getGrade(index);
+  }
+
+  public void submitGrade(Grade grade) {
+    int index = this.getGradeIndex(grade.getId());
+    if (index == Constants.NOT_FOUND) {
+      this.addGrade(grade);
+    } else {
+      this.updateGrade(index, grade);
+    }
+  }
 }
