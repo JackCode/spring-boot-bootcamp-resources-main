0ff19614c8978627942912966140db1ddb949d92
Jackson Weber <jackson@jackcode.com>
2023-02-19 16:11:20 -0500

    rest api

--- 08. rest-api/workbook 8.3/starter/contacts/src/main/java/com/ltp/contacts/ApplicationExceptionHandler.java
+++ 08. rest-api/workbook 8.3/starter/contacts/src/main/java/com/ltp/contacts/ApplicationExceptionHandler.java
@@ -1,18 +1,35 @@
 package com.ltp.contacts;
 
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.ltp.contacts.exception.ContactNotFoundException;
 import com.ltp.contacts.exception.ErrorResponse;
 
 @ControllerAdvice
-public class ApplicationExceptionHandler {
-    @ExceptionHandler(ContactNotFoundException.class)
-    public ResponseEntity<Object> handleContactNotFoundException(ContactNotFoundException ex) {
-      ErrorResponse error = new ErrorResponse(ex.getLocalizedMessage());  
-      return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+  @ExceptionHandler(ContactNotFoundException.class)
+  public ResponseEntity<Object> handleContactNotFoundException(ContactNotFoundException ex) {
+    ErrorResponse error = new ErrorResponse(ex.getLocalizedMessage());
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
+      HttpStatus status, WebRequest request) {
+    List<ObjectError> objErrors = ex.getBindingResult().getAllErrors();
+    for (ObjectError error : objErrors) {
+      System.out.println(error.getDefaultMessage());
     }
+    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+  }
 }
